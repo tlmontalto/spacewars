9792f34d7cf399c00852aecd3b4929923071a8cb
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-10-14 12:05:07 -0400

    began psuedocode outline, created ship console div

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('App Connected.')
+
+// TODO
+
+// Create console on front end
+// Create our spaceship class with attributes name, hull, firepower, accuracy
+// Create alien spaceship class with same hull, firepower, accuracy
+// Create start prompt on screen, with button to accept or decline
+// Generate alien spaceship attack, options to attack it or retreat
+// Generate alien spaceship retaliation
+// On screen prompt for hit/miss + damage done
+// On screen health bar for our and enemy ship
+
+const console = document.querySelector('.information')
